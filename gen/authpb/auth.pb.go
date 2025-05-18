91cd77d0ac8e34fe6bedbadfa8813c11c2667ce8
shahal <shahalsmn@gmail.com>
2025-05-18 21:25:40 +0530

    changes made in auth.proto

--- gen/authpb/auth.pb.go
+++ gen/authpb/auth.pb.go
@@ -646,7 +646,8 @@ func (x *EmployerLoginRequest) GetPassword() string {
 type EmployerLoginResponse struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
 	Id            int64                  `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
-	Message       string                 `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
+	Token         string                 `protobuf:"bytes,2,opt,name=token,proto3" json:"token,omitempty"`
+	Message       string                 `protobuf:"bytes,3,opt,name=message,proto3" json:"message,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -686,6 +687,13 @@ func (x *EmployerLoginResponse) GetId() int64 {
 		return x.Id
 	}
 	return 0
+}
+
+func (x *EmployerLoginResponse) GetToken() string {
+	if x != nil {
+		return x.Token
+	}
+	return ""
 }
 
 func (x *EmployerLoginResponse) GetMessage() string {
@@ -1944,10 +1952,11 @@ const file_Auth_auth_proto_rawDesc = "" +
 	"\amessage\x18\x02 \x01(\tR\amessage\"H\n" +
 	"\x14EmployerLoginRequest\x12\x14\n" +
 	"\x05email\x18\x01 \x01(\tR\x05email\x12\x1a\n" +
-	"\bpassword\x18\x02 \x01(\tR\bpassword\"A\n" +
+	"\bpassword\x18\x02 \x01(\tR\bpassword\"W\n" +
 	"\x15EmployerLoginResponse\x12\x0e\n" +
-	"\x02id\x18\x01 \x01(\x03R\x02id\x12\x18\n" +
-	"\amessage\x18\x02 \x01(\tR\amessage\".\n" +
+	"\x02id\x18\x01 \x01(\x03R\x02id\x12\x14\n" +
+	"\x05token\x18\x02 \x01(\tR\x05token\x12\x18\n" +
+	"\amessage\x18\x03 \x01(\tR\amessage\".\n" +
 	"\x16EmployerProfileRequest\x12\x14\n" +
 	"\x05token\x18\x01 \x01(\tR\x05token\"\x8a\x02\n" +
 	"\x17EmployerProfileResponse\x12\x0e\n" +
